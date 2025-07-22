ad42059f196641806bbeb052d2c275178bf0e5fd
tubeearn <wwwsanjeevbarh95@gmail.com>
2025-07-22 21:25:03 +0530

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to TubeEarn</h1>
+      <p>Your smart video earning platform is live!</p>
+    </div>
+  );
+}
+
+export default App;
